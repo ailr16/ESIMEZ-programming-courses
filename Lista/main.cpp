cd70843cc58b9f7176b5c4e2040f9e3baeee9397
Angel Iván Lozano Ramírez <angel_ivan_5@outlook.com>
2018-09-13 17:48:28 -0500

    Lista v2

--- Lista/main.cpp
+++ Lista/main.cpp
@@ -6,16 +6,36 @@ using namespace std;
 int main()
 {
     ListaS A;
-    A.inserta(3);
-    A.inserta(6);
-    A.inserta(13);
-    A.inserta(9);
-    A.inserta(12);
-    A.inserta(2);
-    A.inserta(11);
-    A.inserta(10);
+
     A.inserta(1);
+    A.inserta(10);
+    A.inserta(0);
+    A.inserta(5);
+    A.inserta(15);
 
     A.muestraTusDatos();
+    cout << endl;
+    A.borra(-1);
+
+    A.muestraTusDatos();
+    cout << endl;
+    A.borra(0);
+
+    A.muestraTusDatos();
+    cout << endl;
+    A.borra(30);
+
+    A.muestraTusDatos();
+    cout << endl;
+    A.borra(15);
+
+    A.muestraTusDatos();
+    cout << endl;
+    A.borra(3);
+
+    A.muestraTusDatos();
+    cout << endl;
+    A.borra(5);
+
     return 0;
 }
