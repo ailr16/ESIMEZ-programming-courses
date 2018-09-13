14da97d14e151ad287f0ea48e79904c85ccf2ef4
Angel Iván Lozano Ramírez <angel_ivan_5@outlook.com>
2018-09-12 21:32:08 -0500

    ListaS
    Completo el método inserta(int v)

--- Lista/main.cpp
+++ Lista/main.cpp
@@ -6,8 +6,14 @@ using namespace std;
 int main()
 {
     ListaS A;
+    A.inserta(3);
+    A.inserta(6);
+    A.inserta(9);
+    A.inserta(12);
     A.inserta(2);
-    A.inserta(1);
+    A.inserta(13);
+    A.inserta(11);
+    A.inserta(10);
 
     A.muestraTusDatos();
     return 0;
