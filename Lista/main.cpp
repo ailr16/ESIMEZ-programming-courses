2576dafafab44b8872f238bcad948c469eaccf56
Angel Iván Lozano Ramírez <angel_ivan_5@outlook.com>
2018-09-12 21:33:07 -0500

    Clase ListaS
    Completo método inserta(_int)

--- Lista/main.cpp
+++ Lista/main.cpp
@@ -8,12 +8,13 @@ int main()
     ListaS A;
     A.inserta(3);
     A.inserta(6);
+    A.inserta(13);
     A.inserta(9);
     A.inserta(12);
     A.inserta(2);
-    A.inserta(13);
     A.inserta(11);
     A.inserta(10);
+    A.inserta(1);
 
     A.muestraTusDatos();
     return 0;
