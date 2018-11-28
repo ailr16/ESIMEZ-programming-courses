d4b6a19a20d646a5345b41bb0da23deb6fd72820
Angel Iván Lozano Ramírez <angel_ivan_5@outlook.com>
2018-11-28 14:32:51 -0600

    practica7

--- practica7/main.cpp
+++ practica7/main.cpp
@@ -14,16 +14,25 @@ int main()
         Q[MF][MC],
         m;
 
+    string QR[MF][MC];
+
+
     cout << "Ingresa la dimension de la matriz de pesos (mxm)= ";
     cin >> m;
+    inicializaMatrizQR(QR,m);
 
     leeMatriz(W,m);
     system("cls");
     cout << "Matriz W=";
     imprimeMatriz(W,m);
     generaMatrizQ(W,Q,m);
-    cout << "Matriz Q=";
-    comparaElementos(Q,A,m);
+    generaMatrizA(W,A,m);
+    cout << "Matriz Q_0=";
     imprimeMatriz(Q,m);
+    cout << "Matriz A_0=";
+    imprimeMatriz(A,m);
+    comparaElementos(Q,A,QR,m);
+    cout << "Matriz QR=";
+    imprimeMatrizCadena(QR,m);
     return 0;
 }
