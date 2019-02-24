c9d8b69bf2367ace2d34b78890f8be9b874dd5ed
ailr16 <angel_ivan_5@outlook.com>
2019-02-23 18:18:06 -0600

    practica5 (Individual)
    Version Final

---
+++ practica5Num16/practica5Num16/MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+
+using namespace System;;
+using namespace System::Windows::Forms;
+
+void Main(void) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	practica5Num16::MyForm form;			//Cambiar "CLRWindowsForms por el nombre del proyecto"
+	Application::Run(%form);
+}
