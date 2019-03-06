d6911be7ebd72e5e1760da1dab2955c1c0739a1e
ailr16 <angel_ivan_5@outlook.com>
2019-03-05 18:49:54 -0600

    actualizacion (creacion de directorios)

---
+++ segundoParcial/practicaSsel/practicaSsel/practicaSsel.cpp
@@ -0,0 +1,22 @@
+// practicaSsel.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "ssel.h"
+#include <complex>
+
+int main()
+{
+	int numEcuaciones;
+	cout << "Ingrese numero de ecuaciones: ";
+	cin >> numEcuaciones;
+	ssel mat(numEcuaciones);
+	mat.leeMat();
+	cout << "Matriz ingresada: " << endl;
+	mat.impMat();
+	mat.Gauss();
+	cout << "Resultados: " << endl;
+	mat.impMatResultados();
+}
+
---
+++ segundoParcial/practicaSsel/practicaSsel/ssel.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+class ssel {
+	private:
+		int m, n;
+		//complex<double> **a, *z;
+		double **a, *z;
+	public:
+		ssel(int m){
+			this->m = m;
+			n = m + 1;
+			//z = new complex<double>[m];
+			//a = new complex<double>*[m];
+			z = new double[m];
+			a = new double*[m];
+			for (int i = 0; i < m; i++) {
+				//a[i] = new complex<double>[n];
+				a[i] = new double[n];
+			}
+		}
+		void leeMat(void) {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					cout << "Ingrese [" << i << "][" << j << "]";
+					cin >> a[i][j];
+				}
+			}
+		}
+		void impMat(void) {
+			for (int k = 0; k < m; k++) {
+				for (int i = 0; i < n; i++) {
+					cout << a[k][i] << "\t";
+				}
+				cout << endl;
+			}
+		}
+		void impMatResultados(void) {
+			for (int i = 0; i < m; i++) {
+				cout << "z[" << i+1 << "] = " << z[i] << endl;
+			}
+		}
+		void Gauss() {
+			int n = m + 1;
+			double pivote;
+			double cero;
+			double s;
+			for (int i = 0; i < m; i++) {
+				pivote = a[i][i];
+				for (int j = 0; j < n; j++) {
+					a[i][j] = a[i][j] / pivote;
+				}
+				for (int k = 0; k < m; k++) {
+					if (k > i) {
+						cero = a[k][i];
+						for (int j = i; j < n; j++) {
+							a[k][j] = a[k][j] - cero * a[i][j];
+						}
+					}
+				}
+			}
+			z[m-1] = a[m-1][n-1];
+			for (int i = 0; i < m; i++) {
+				s = 0;
+				for (int l = i + 1; l < m; l++) {
+					s = s + a[i][l] * z[l];
+				}
+				z[i] = a[i][n-1] - s;
+			}
+		}
+
+};
