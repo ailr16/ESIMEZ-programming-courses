86cddc3db7039c4f0ef5e5dc4364bda6eb3d149d
ailr16 <angel_ivan_5@outlook.com>
2019-03-07 21:12:14 -0600

    practicaSselWindows
    - Bloqueada opcion ajustar tamaño de la ventana y bloqueado botom maximizar

--- segundoParcial/practicaSselWindows/practicaSselWindows/MyForm.h
+++ segundoParcial/practicaSselWindows/practicaSselWindows/MyForm.h
@@ -264,6 +264,8 @@ namespace practicaSselWindows {				//Cambiar CLRWindowsForms por nombre del proy
 			this->Controls->Add(this->groupBox3);
 			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->groupBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
 			this->Name = L"MyForm";
 			this->Text = L"Solucionador de Sistemas de Ecuaciones Lineales";
 			this->groupBox1->ResumeLayout(false);
