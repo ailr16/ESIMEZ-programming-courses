a344c3fafad380e7b295bb587258cf3c0e479891
ailr16 <angel_ivan_5@outlook.com>
2019-03-19 17:36:36 -0600

    practica8 (individual)
    Verion Final

--- segundoParcial/practica8Num16/practica8Num16/ssel.h
+++ segundoParcial/practica8Num16/practica8Num16/ssel.h
@@ -25,6 +25,9 @@ class ssel {
 				a[i] = new T[n];
 			}
 		}
+		void modificaMatriz(double** b) {
+			this->a = b;
+		}
 		void leeMat(void) {
 			for (int i = 0; i < m; i++) {
 				for (int j = 0; j < n; j++) {
